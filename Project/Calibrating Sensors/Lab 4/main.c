7be9bf4d557fc34d76a27b228840412054231beb
BennettUVicSubClub <diysandvich@gmail.com>
2024-11-09 19:29:04 -0800

    Added stuff from the project skeleton

--- Project/Calibrating Sensors/Lab 4/main.c
+++ Project/Calibrating Sensors/Lab 4/main.c
@@ -5,13 +5,15 @@
 #include "pwm.h"
 
 // define the global variables that can be used in every function ===========
-volatile uint16_t ADC_result = 455;
-volatile unsigned int ADC_result_flag;
-volatile unsigned char motorDir = 0x00;
-volatile unsigned char motorState = 0x01;
+volatile uint16_t ADC_result = 455; //Value our reflectivity sensor displays normally
+volatile unsigned int ADC_result_flag; //Sets when ADC conversion is finished
+volatile unsigned char motorDir = 0x00; //Direction of Stepper Motor
+volatile unsigned char motorState = 0x01; 
+volatile char STATE; //Program Polling State
 
 int main()
 {
+	STATE = 0;
 	timer8MHz();//setup the chip clock to 8 MHz
 	DDRL = 0xFF;//sets debug lights to output
 	DDRB = 0x03;//sets D0 and D1 to output
@@ -27,7 +29,7 @@ int main()
 	mTimer(500);
 	LCDClear();
 	LCDWriteString("loading");
-	mTimer(2000);
+	mTimer(500);
 	LCDClear();
 	LCDWriteString("ADC Value:");
 	LCDGotoXY(0,1);
@@ -50,23 +52,70 @@ int main()
 	sei();
 	
 	pwm();
-	pwmSet(102);
+	pwmSet(102); //40% Duty cycle: 255*0.4 = 102
 
 	// initialize the ADC, start one conversion at the beginning ============
 	ADCSRA |= _BV(ADSC);
 	
-	while (1)
-	{
-		PORTL = (PINE & 0b00100000) << 2;
-		if (ADC_result_flag)
-		{
-			LCDGotoXY(10,0);
-			LCDWriteInt(ADC_result,5);
-			ADC_result_flag = 0x00;
-			mTimer(20);
-			ADCSRA |= _BV(ADSC);
-		}
+	goto POLLING_STAGE;
+	
+	POLLING_STAGE:
+	PORTC |= 0xF0;	// Indicates this state is active
+	switch(STATE){
+		case (0) :
+		goto POLLING_STAGE;
+		break;	//not needed but syntax is correct
+		case (1) :
+		goto MAGNETIC_STAGE;
+		break;
+		case (2) :
+		goto REFLECTIVE_STAGE;
+		break;
+		case (3) :
+		goto BUCKET_STAGE;
+		break;
+		case (5) :
+		goto END;
+		default :
+		goto POLLING_STAGE;
+	}//switch STATE
+	
+	MAGNETIC_STAGE:
+	// Do whatever is necessary HERE
+	PORTC = 0x01; // Just output pretty lights know you made it here
+	//Reset the state variable
+	STATE = 0;
+	goto POLLING_STAGE;
+
+	REFLECTIVE_STAGE:
+
+	PORTL = (PINE & 0b00100000) << 2;
+	if (ADC_result_flag){
+		LCDGotoXY(10,0);
+		LCDWriteInt(ADC_result,5);
+		ADC_result_flag = 0x00;
+		mTimer(20);
 	}
+	if ()
+	
+	PORTC = 0x04; // Just output pretty lights know you made it here
+	//Reset the state variable
+	STATE = 0;
+	goto POLLING_STAGE;
+		
+	BUCKET_STAGE:
+	// Do whatever is necessary HERE
+	PORTC = 0x08;
+	//Reset the state variable
+	STATE = 0;
+	goto POLLING_STAGE;
+		
+	END:
+	// The closing STATE ... how would you get here?
+	PORTC = 0xF0;	// Indicates this state is active
+	// Stop everything here...'MAKE SAFE'
+	return(0);
+
 } // end main
 
 // sensor switch: Active HIGH starts AD conversion ==========================
