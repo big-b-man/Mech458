df3a97357c57dee52763744db7f6abef0d5f0048
BennettUVicSubClub <diysandvich@gmail.com>
2024-11-15 01:40:48 -0800

    main.c modifications, changed interupts and some other stuff.

--- Project/The real deal/main.c
+++ Project/The real deal/main.c
@@ -6,7 +6,7 @@
 
 // define the global variables that can be used in every function ===========
 volatile uint16_t ADC_result = 455;
-volatile unsigned int ADC_result_flag;
+volatile unsigned int ADC_result_flag = 1;
 volatile unsigned char motorState = 0x00;
 volatile char STATE = 0;
 
@@ -37,35 +37,67 @@ int main()
 	cli(); // disable all of the interrupt ==================================
 
 	// config the external interrupt ========================================
-	EIMSK |= ( _BV(INT2));                     // enable INT2
-	EICRA |= ( _BV(ISC21));                    // rising edge interrupt
+	EIMSK |= (1 << INT2) | (1 << INT5);        // enable INT2 and INT5
+	EICRA |= (1 << ISC21);					   // falling edge interrupt for INT2
+	EICRB |= (1 << ISC50);					   // any edge interrupt for INT5
 
 	// config ADC ===========================================================
 	// by default, the ADC input (analog input) is set to ADC0 / PORTF0
-	ADCSRA |= _BV(ADEN);                       // enable ADC
-	ADCSRA |= _BV(ADIE);                       // enable interrupt of ADC
-	ADMUX |= _BV(REFS0);           //AVCC with external capacitor at AREF pin
+	ADCSRA |= (1 << ADEN);                       // enable ADC
+	ADCSRA |= (1 << ADIE);                       // enable interrupt of ADC
+	ADMUX  |= (1 << REFS0);						 //AVCC with external capacitor at AREF pin
 
 	// sets the Global Enable for all interrupts ============================
 	sei();
 	
+	
+	//pwm setup to 40% duty cycle
 	pwm();
 	pwmSet(102);
 
-	while (1){
-		while (PINE == 0b00100000){
-			ADCSRA |= _BV(ADSC);
-			mTimer(20);
-		}
-	}
-	
+	goto POLLING_STAGE;
+
+	// POLLING STATE
+	POLLING_STAGE:
+	PORTL = (PINL5 < 1);	// Indicates this state is active
+	switch(STATE){
+		case (0) :
+		goto POLLING_STAGE;
+		break;	//not needed but syntax is correct
+		case (1) :
+		goto REFLECTIVE_STAGE;
+		break;
+		case (2) :
+		goto BUCKET_STAGE;
+		break;
+		case (3) :
+		goto END;
+		default :
+		goto POLLING_STAGE;
+	}//switch STATE
+
+	REFLECTIVE_STAGE:
+	PORTL = (PINL6 << 1);//debug light to show we're in the reflective stage
+	if(ADC_result_flag == 1){
+	ADCSRA |= (1 << ADSC);//start a new ADC convert if last one is done
+	ADC_result_flag = 0;
+	} //do nothing if an ADC convert is in progress
+	STATE = 0;
+	goto POLLING_STAGE;
+
+	BUCKET_STAGE:
+	// Do whatever is necessary HERE
+	PORTC = 0x08;
+	//Reset the state variable
+	STATE = 0;
+	goto POLLING_STAGE;
+
 	END:
 	// The closing STATE ... how would you get here?
-
+	PORTC = 0xF0;	// Indicates this state is active
 	// Stop everything here...'MAKE SAFE'
 	return(0);
 
-}
 } // end main
 
 // sensor switch: Active HIGH starts AD conversion ==========================
@@ -74,9 +106,17 @@ ISR(INT2_vect)
 	ADC_result = 999;
 }
 
-ISR(INT4_vect)
+ISR(INT5_vect)// Interrupt 5, Triggered by the optical sensor next to the reflectivity sensor
 {
-	//interupt code
+    mTimer(20);//de-bouncing
+	if (PINE & (1 << PINE5)) {
+	    //If pin is high, enter polling state
+		STATE = 1;
+	    } else {
+	    // INT5 pin is low
+	    
+		//enque code
+    }
 }
 
 // the interrupt will be triggered if the ADC is done =======================
@@ -85,9 +125,14 @@ ISR(ADC_vect)
 	uint16_t ADC_result_last = ADC_result;
 	ADC_result = ADCL;
 	ADC_result |= (ADCH && 0x03) << 8;
-	if((ADC_result < ADC_result_last)/* && (ADC_result > (ADC_result_last-20))*/){
+	if((ADC_result < ADC_result_last)){//gets us the lowest value read by the reflectivity sensor
 		} else {
 		ADC_result = ADC_result_last;
 	}
 	ADC_result_flag = 1;
+}
+
+ISR(BADISR_vect)
+{
+	PORTL = 0xF0;//light up everything to let us know it's screwed
 }
